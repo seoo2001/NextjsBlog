3ffeb68dd0be4fbaa4680885a63f207db8676b02
seoo2001 <seoo2001@naver.com>
2023-05-31 15:27:47 +0900

    init

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,8 +2,8 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'Personal Blog',
+  author: 'DongJoon Seo',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
